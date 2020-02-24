867e9e86196048ea79a48093a26210cfe426b8e1
RvanB <rvanbronkhorst@gmail.com>
2020-02-23 16:27:15 -0800

    Animate text, calculate kerning

--- public/script.js
+++ public/script.js
@@ -1,7 +1,51 @@
 initLetters();
 
-window.onload = function() {
-    animateLetters();
+function hoverLetters() {
+    let letters = document.getElementsByClassName("letter");
+    
+    for (let i = 0; i < letters.length; i++) {
+        let letter = letters[i];
+        letter.style.backgroundColor = "yellow";
+    }
+}
+
+function getRowString(row) {
+    let string = "";
+    for (let i = 0; i < row.children.length; i++) {
+        string = string + row.children[i].firstChild.innerHTML;
+    }
+    console.log(string);
+    return string;
+}
+
+function kern() {
+    let kerningCalculator = document.getElementById("kerningCalculator");
+    let rows = document.getElementsByClassName("letterRow");
+
+    for (let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+        let string = getRowString(row);
+        kerningCalculator.innerHTML = string[0];
+        let totalWidth = kerningCalculator.offsetWidth;
+        for (let j = 0; j < row.children.length - 1; j++) {
+            kerningCalculator.innerHTML = string[j];
+            let aWidth = kerningCalculator.offsetWidth;
+            
+            kerningCalculator.innerHTML = string[j + 1];
+            let bWidth = kerningCalculator.offsetWidth;
+
+            kerningCalculator.innerHTML = string.substring(j, j + 2);
+            let kernedWidth = kerningCalculator.offsetWidth;
+
+            let bMarginLeft = kernedWidth - (aWidth + bWidth);
+            row.children[j + 1].style.marginLeft = bMarginLeft + "px";
+
+            totalWidth = totalWidth + bWidth;
+        }
+        row.style.width = totalWidth + "px";
+    }
+
+    kerningCalculator.parentNode.removeChild(kerningCalculator);
 }
 
 function initLetters() {
@@ -16,6 +60,7 @@ function initLetters() {
             letter.firstChild.style.left = "50px";
         }
     }
+    kern();
 }
 
 function animateLetters() {
@@ -28,5 +73,9 @@ function animateLetters() {
             letter.firstChild.style.left = "0px";
         }
     }
+}
+
+window.onload = function() {
+    animateLetters();
 }
 
